b09067c9ea2c6603b71746b5414db26fda30db01
Rachelle Phipps <rachelle.s.phipps@gmail.com>
2023-04-29 17:10:25 -0500

    added AES python file

---
+++ AES.py
@@ -0,0 +1,29 @@
+import json
+from base64 import b64encode, b64decode
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+
+result = ""
+
+def encryption(data):
+    key = get_random_bytes(16)
+    cipher = AES.new(key, AES.MODE_CFB)
+    ct_bytes = cipher.encrypt(data)
+    iv = b64encode(cipher.iv).decode('utf-8')
+    ct = b64encode(ct_bytes).decode('utf-8')
+    with open('data.json', 'w') as file:
+        json.dump({'iv':iv, 'ciphertext':ct}, file)
+    result = json.dumps({'iv':iv, 'ciphertext':ct})
+
+
+def decryption():
+    try:
+        b64 = json.loads(result)
+        iv = b64decode(b64['iv'])
+        ct = b64decode(b64['ciphertext'])
+        cipher = AES.new(key, AES.MODE_CFB, iv=iv)
+        pt = cipher.decrypt(ct)
+        #print("The message was: ", pt)
+        return pt
+    except (ValueError, KeyError):
+       return "Decryption Error"
