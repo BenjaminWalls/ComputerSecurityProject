852fbf55c03be9b1ba02e5833253b3781d50af67
Rachelle Phipps <rachelle.s.phipps@gmail.com>
2023-04-30 16:18:24 -0500

    changed things

--- app.py
+++ app.py
@@ -16,23 +16,16 @@ def handle_message(message):
     print("Received message: " + message)
     data = bytes(message, encoding= 'utf-8')
     
-    
     key = get_random_bytes(16)
-    # change key to be the message padded?
-
 
     cipher = AES.new(key, AES.MODE_CFB)
     ct_bytes = cipher.encrypt(data)
     iv = b64encode(cipher.iv).decode('utf-8')
     ct = b64encode(ct_bytes).decode('utf-8')
-    with open('text.json', 'w') as file:
-        json.dump({'iv':iv, 'ciphertext':ct}, file)
     result = json.dumps({'iv':iv, 'ciphertext':ct})
 
     
-    with open('text.json', 'r') as myfile:
-        text=myfile.read()
-    b64 = json.loads(text)
+    b64 = json.loads(result)
     iv = b64decode(b64['iv'])
     ct = b64decode(b64['ciphertext'])
     cipher = AES.new(key, AES.MODE_CFB, iv=iv)
